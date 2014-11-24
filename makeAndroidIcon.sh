2a6e4a76c0c9fc46c6caf4340bbbb3c45a4161cd
Carlos Fonseca <carlosefonseca@gmail.com>
2014-11-24 23:21:27 +0000

    + usage to makeAndroidIcon.sh

--- makeAndroidIcon.sh
+++ makeAndroidIcon.sh
@@ -3,5 +3,10 @@
 # $1 original
 # $2 destination e.g. drawable/launcher_icon.png
 
-roundCorners.sh $1 $2
-resizeAndroidIcon.sh $2
+if [[ $# -eq 2 ]]; then
+	roundCorners.sh "$1" "$2"
+	resizeAndroidIcon.sh "$2"
+else
+	echo "Usage:"
+	echo "$0 original_image destination_image"
+fi
