0ec7a88dcd9cca923eec0c81f21aeee6247a34ff
Carlos Fonseca <carlosefonseca@gmail.com>
2015-01-05 22:39:25 +0000

    roundCorners.sh - less dramatic round corners

--- roundCorners.sh
+++ roundCorners.sh
@@ -2,7 +2,7 @@
 
 if [[ $# -eq 2 ]]; then
 	mkdir -p "$(dirname $2)"
-	convert "$1" \( +clone -alpha extract -draw 'fill black polygon 0,0 0,200 200,0 fill white circle 200,200 200,0' \( +clone -flip \) -compose Multiply -composite \( +clone -flop \) -compose Multiply -composite \) -alpha off -compose CopyOpacity -composite "$2"
+	convert "$1" \( +clone -alpha extract -draw 'fill black polygon 0,0 0,100 100,0 fill white circle 100,100 100,0' \( +clone -flip \) -compose Multiply -composite \( +clone -flop \) -compose Multiply -composite \) -alpha off -compose CopyOpacity -composite "$2"
 else
 	echo "Usage:"
 	echo "$0 start_image_path result_image_path"
